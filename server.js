0ae99ffc6e0b673661ab0cc7efc4fc69befef699
lautarog1596 <lautarog1596@gmail.com>
2022-02-23 15:08:30 -0300

    Add API for empty date

--- server.js
+++ server.js
@@ -25,7 +25,16 @@ app.get("/api/hello", function (req, res) {
 });
 
 
-// 1. The API endpoint is GET [project_url]/api/timestamp/:date_string
+// 3. if the date string is empty it should be equivalent to trigger new Date(), i.e. the service uses the current timestamp
+app.get("/api", function(req, res) {
+  let now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString()
+  });
+});
+
+// 1. The API endpoint is GET [project_url]/api/:date_string
 app.get("/api/:date_string", function(req, res) {
   var date_string = req.params.date_string;
   // 2. A date string is valid if can be successfully parsed by new Date(date_string).
@@ -33,11 +42,6 @@ app.get("/api/:date_string", function(req, res) {
   // In our test we will use date strings compliant with ISO-8601 (e.g. "2016-11-20") because this 
   // will ensure an UTC timestamp
   var date = new Date(date_string);
-
-  // 3. if the date string is empty it should be equivalent to trigger new Date(), i.e. the service uses the current timestamp
-  if (date_string === "") {
-    date = new Date();
-  }
 
   // 4. if the date string is valid the api returns a JSON having the structure:
   // {"unix": <date.getTime()>, "utc": <date.toUTCString()>}
